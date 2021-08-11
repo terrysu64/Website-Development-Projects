fe487b0fc50c4e5ab429935d9a4cac954641bf90
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-08-11 18:06:45 -0400

    robofriends functionality updates

--- Robofriends/src/App.js
+++ Robofriends/src/App.js
@@ -1,28 +1,35 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component} from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+
+	constructor() {
+		super()
+		this.state = {
+			'robots': robots,
+			'searchfield': ''
+		}
+	}
+
+	//any methods that arent prebuilt need the special => syntax
+	onSearchChange = (event) => {
+		this.setState({ 'searchfield' : event.target.value})
+	}
+
+	render() {
+		const filteredRobots = this.state.robots.filter(robot => {
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+		})
+		return (
+			<div className='tc'>
+				<h1>Robofriends</h1>
+				<SearchBox searchChange={this.onSearchChange}/>
+				<CardList robots={filteredRobots}/>
+			</div>
+		);
+	};
+};
 
 export default App;
