d69a7a9cb996efea07df16653f4f60e6e84f12e6
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-08-18 15:51:45 -0400

    added comments to index.js

--- Personal Simplefolio Website/src/index.js
+++ Personal Simplefolio Website/src/index.js
@@ -1,3 +1,7 @@
+//Date: August 13, 2021
+//Author: Terry Su
+//Purpose: A full-stack personal website to showcase my portfolio and some of my CS projects
+
 import initApp from "./js/index";
 import "./style/main.scss";
 
