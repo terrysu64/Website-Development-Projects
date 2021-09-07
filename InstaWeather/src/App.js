e0db4fb626c65a02efe9dd0914805abd5ecb923f
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-09-07 16:20:44 -0400

    new project

---
+++ InstaWeather/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    console.log('app is running!')
+  }
+
+  render() {
+    return (
+      <div class='app'>
+            {/*
+            nav bar*/}
+      </div>
+    )
+  }
+
+}
+
+export default App;
