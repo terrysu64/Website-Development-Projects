ab78327d675ee7ab72db6c03b735bee4478d08c6
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2022-07-06 16:11:33 -0400

    node-client init

---
+++ PT Monitor/node-client/index.js
@@ -0,0 +1,54 @@
+//local preformace data
+
+import os from "os"
+
+const getPreformance = async () => {
+  const cpus = os.cpus()
+  const cpuModel = cpus[0].model  
+  const numCores = cpus.length
+  const cpuSpeed = cpus[0].speed
+  const cpuLoad = await getCpuLoad().then((data) => data)
+
+  const osType = os.type()
+  const upTime = os.uptime()
+  const freeMemory = os.freemem()
+  const totalMemory = os.totalmem()
+  const memoryUsage = Math.floor((totalMemory - freeMemory) / totalMemory*100) / 100
+  return {freeMemory,totalMemory,memoryUsage,osType,upTime,cpuModel,numCores,cpuSpeed,cpuLoad}
+}
+
+
+//AVG CPU IDLE/TOTAL USAGE TIME 
+const cpuAvg = () => {
+  const cpus = os.cpus()
+  let idleMs = 0
+  let totMs = 0
+
+  cpus.forEach((core) => {
+    for (const [type, typeMs] of Object.entries(core.times)) {
+      totMs += typeMs
+    }
+    idleMs += core.times.idle
+  })
+
+  return {
+    idle: idleMs / cpus.length,
+    total: totMs / cpus.length
+  }
+}
+
+//TIMES PROPRETY == TIME SINCE BOOT => DIFF == CPU LOAD SINCE TRACKING STARTED
+const getCpuLoad = () => {
+  return new Promise((resolve, reject) => {
+    const start = cpuAvg()
+    setTimeout(() => {
+      const end = cpuAvg()
+      const idleDiff = end.idle - start.idle
+      const totDiff = end.total - start.total
+      const percentageUsage = 100 - Math.floor(100 * idleDiff / totDiff)
+      resolve(percentageUsage)
+    }, 100)
+  })
+}
+
+getPreformance().then((data) => console.log(data))
