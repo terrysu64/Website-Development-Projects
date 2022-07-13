85d0ca5fc2cbb4819af01aa84ac77cdd4b145121
Terry Su <terrysu64@Terrys-MacBook-Air.local>
2022-07-13 10:40:10 -0400

    rm proj

---
+++ Robofriends/src/__tests__/app.test.js
@@ -0,0 +1,5 @@
+import '@testing-library/jest-dom';
+
+test('numeric ranges', () => {
+    expect(100).toBe(100);
+});
--- shop-x-weeb/src/__tests__/app.test.js
+++
@@ -1,5 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-test('renders learn react link', () => {
-  expect(1).toBeTruthy();
-})
